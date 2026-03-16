87604b6ef9ba83767dfe464d1e29d59637e32bdb
abdellatif guezadi <abdellatifguezadi@gmail.com>
2026-03-16 11:20:26 +0000

    feat: add NgRx for state management and integrate Tailwind CSS
    - Updated package.json to include NgRx effects and store, as well as Tailwind CSS and its dependencies.
    - Created postcss.config.js to configure Tailwind CSS and autoprefixer.
    - Modified app.config.ts to set up NgRx store and effects for authentication.
    - Added environment.ts for environment-specific configurations.
    - Created tailwind.config.js for Tailwind CSS setup.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,22 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { authInterceptor } from './core/interceptors/auth.interceptor';
+import { authReducer } from './features/auth/store/auth.reducer';
+import { AuthEffects } from './features/auth/store/auth.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideStore({ auth: authReducer }),
+    provideEffects([AuthEffects])
   ]
 };

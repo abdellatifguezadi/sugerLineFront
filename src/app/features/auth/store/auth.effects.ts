15ed98407ba198097f0d1c5deeb06d88f653cbae
abdellatif guezadi <abdellatifguezadi@gmail.com>
2026-03-16 11:27:35 +0000

    feat: implement authentication store with actions, effects, reducer, and selectors

---
+++ src/app/features/auth/store/auth.effects.ts
@@ -0,0 +1,36 @@
+import { Injectable, inject } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { of } from 'rxjs';
+import { map, catchError, switchMap } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+import * as AuthActions from './auth.actions';
+
+@Injectable()
+export class AuthEffects {
+  private actions$ = inject(Actions);
+  private authService = inject(AuthService);
+
+  login$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.login),
+      switchMap(({ credentials }) =>
+        this.authService.login(credentials).pipe(
+          map(response => AuthActions.loginSuccess({ response })),
+          catchError(error => of(AuthActions.loginFailure({ error: error.message })))
+        )
+      )
+    )
+  );
+
+  loadUser$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(AuthActions.loadUser),
+      switchMap(() =>
+        this.authService.me().pipe(
+          map(user => AuthActions.loadUserSuccess({ user })),
+          catchError(error => of(AuthActions.loadUserFailure({ error: error.message })))
+        )
+      )
+    )
+  );
+}

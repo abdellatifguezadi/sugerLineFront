997ae96d1090d86d1e394a15355d7d635d204a05
abdellatif guezadi <abdellatifguezadi@gmail.com>
2026-03-16 14:13:18 +0000

    feat: enhance admin dashboard with logout functionality and improve HTML structure

--- src/app/features/admin/admin-dashboard/admin-dashboard.ts
+++ src/app/features/admin/admin-dashboard/admin-dashboard.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import * as AuthActions from '../../auth/store/auth.actions';
 
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css',
 })
-export class AdminDashboardComponent {}
+export class AdminDashboardComponent {
+  private store = inject(Store);
+  private router = inject(Router);
+
+  logout(): void {
+    this.store.dispatch(AuthActions.logout());
+    this.router.navigate(['/login']);
+  }
+}
